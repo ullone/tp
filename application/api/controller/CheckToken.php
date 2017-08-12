ab50e4b120a9f2c664329e6a8e768f6e986b09a1
ullone <ullone@163.com>
2017-08-12 10:48:49 +0800

    fix

--- application/api/controller/CheckToken.php
+++ application/api/controller/CheckToken.php
@@ -14,6 +14,12 @@ class CheckToken
       $timestamp = $_GET['timestamp'];
       $echostr   = $_GET['echostr'];
       $signature = $_GET['signature'];
+      $myFile    = fopen("test.txt","w");
+      fwrite($myFile,$nonce.'\n');
+      fwrite($myFile,$token.'\n');
+      fwrite($myFile,$timestamp.'\n');
+      fwrite($myFile,$echostr.'\n');
+      fwrite($myFile,$signature.'\n');
       // var_dump($echostr);die ;
       //形成数组，然后按字典序排序
       $array = array();
@@ -21,9 +27,10 @@ class CheckToken
       sort($array);
       //拼接成字符串,sha1加密 ，然后与signature进行校验
       $str = sha1( implode( $array ) );
+      fwrite($myFile,$str.'\n');
+      fclose($myFile);
       if( $str == $signature && $echostr ){
           //第一次接入weixin api接口的时候
-          // ob_clean();
           echo $echostr;
           // var_dump($echostr); die ;
         }
