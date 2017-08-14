4eb20d58e1877e13b625c66fae6dce492eb07ac6
ullone <ullone@163.com>
2017-08-14 16:01:43 +0800

    fix

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,18 +1,19 @@
 <?php
 namespace app\index\controller;
 
-use \think\View;
+use \think\Controller;
 use app\api\controller\CheckToken;
 use app\api\controller\UserAuth;
 
-class Index
+class Index extends \think\Controller
 {
     public function index()
     {
       // $first = new CheckToken();
       // $first ->index();
       $a = '<a href="https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx1088ddeead7c4aa7&redirect_uri=http://rewrite.ullone.com/api/Oauth&response_type=code&scope=snsapi_base&state=123#wechat_redirect"></a>';
-      return \think\View('index',['a' => $a]);
+      $this->assign('a',$a);
+      return $this->fetch();
       // $oAuth = new UserAuth();
       // $oAuth -> index();
     }
