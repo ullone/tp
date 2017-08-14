1f1e332a19bd2e2c1a50b40a420ac2787299a2d7
ullone <ullone@163.com>
2017-08-14 16:18:07 +0800

    fix

--- application/api/controller/Oauth.php
+++ application/api/controller/Oauth.php
@@ -17,7 +17,6 @@ class Oauth {
   private $state;
   public function index () {
     // $this->options = Tool::getOptions();
-    exit('haha');die;
     $this->authCheck();
   }
 
@@ -27,7 +26,7 @@ class Oauth {
     $jsondecode = json_decode($weixin); //对JSON格式的字符串进行编码
     $array = get_object_vars($jsondecode);//转换成数组
     $openid = $array['openid'];//输出openid
-    return $openid;
+    exit($openid);
     // if(empty($_GET['url']))
     //   $this->state = 'null';
     // else $this->state = $_GET['url'];
