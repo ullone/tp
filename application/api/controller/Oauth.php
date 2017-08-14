450b2d7860b22ac3d38cbfde2a39069d703c7cf3
ullone <ullone@163.com>
2017-08-14 17:34:09 +0800

    fix

--- application/api/controller/Oauth.php
+++ application/api/controller/Oauth.php
@@ -22,6 +22,8 @@ class Oauth {
 
   private function authCheck () {
     $code = $_GET['code'];//获取code
+    $scene = $_GET['scene'];
+    exit($scene);
     $weixin =  file_get_contents("https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx1088ddeead7c4aa7&secret=f8779402d2d919717ae7fe8a4fc26230&code=".$code."&grant_type=authorization_code");//通过code换取网页授权access_token
     $jsondecode = json_decode($weixin); //对JSON格式的字符串进行编码
     $array = get_object_vars($jsondecode);//转换成数组
